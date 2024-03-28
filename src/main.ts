a52594f1d0de6618ab9e84465c93dcc111b9181f
Jake <jacobminor@hotmail.co.uk>
2024-03-28 18:47:14 +0000

    Add Vue Router dependency

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
+//@ts-ignore
+import { createRouter, createWebHistory } from 'vue-router/auto'
+//@ts-ignore
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHistory()
+})
+
+createApp(App).use(router).mount('#app')
