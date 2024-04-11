663e98f11b6bb6e817de409fdfbf70b9b47ffcb5
Jacob Minor <jacob.minor@3squared.com>
2024-04-11 13:36:33 +0100

    Add validation to Register game form

--- src/components/game/RegisterModal.vue
+++ src/components/game/RegisterModal.vue
@@ -5,30 +5,34 @@
       <div class="mt-7">
         <h2 class="font-semibold">Setting</h2>
         <div class="flex flex-col w-full">
-          <FormField label="Location" class="mt-6">
-            <Dropdown class="w-full" v-model="game.location" :options="getLocations" option-label="name"/>
+          <FormField label="Location" class="mt-6" :error="errors.location">
+            <Dropdown class="w-full" v-model="location" :options="getLocations" option-label="name"
+                      :invalid="errors.location"/>
           </FormField>
-          <FormField label="Colour of Ball" class="mt-7">
-            <Dropdown class="w-full" v-model="game.player1BallColour" :disabled="game.location === null" :options="ballColours"/>
+          <FormField label="Colour of Ball" class="mt-7" :error="errors.player1BallColour">
+            <Dropdown class="w-full" v-model="player1BallColour" :disabled="location === null" :options="ballColours"
+                      :invalid="errors.player1BallColour"/>
           </FormField>
         </div>
       </div>
 
       <div class="mt-8">
         <h2 class="font-semibold">Game Details</h2>
-        <FormField label="Player 1" class="mt-6 w-full" v-if="!isAuthenticated">
-          <Dropdown class="w-full" editable v-model="game.player1" :options="opponents" option-label="name"/>
+        <FormField label="Player 1" class="mt-6 w-full" v-if="!isAuthenticated" :error="errors.player1">
+          <Dropdown class="w-full" editable v-model="player1" :options="opponents" option-label="name"
+                    :invalid="errors.player1"/>
         </FormField>
-        <FormField :label="isAuthenticated ? 'Opponent' : 'Player 2'" class="mt-7 w-full">
-          <Dropdown class="w-full" editable v-model="game.player2" :options="opponents" option-label="name"/>
+        <FormField :label="isAuthenticated ? 'Opponent' : 'Player 2'" class="mt-7 w-full" :error="errors.player2">
+          <Dropdown class="w-full" editable v-model="player2" :options="opponents" option-label="name"
+                    :invalid="errors.player2"/>
         </FormField>
         <div class="flex mt-8">
-          <FormField label="Home" class="mr-5">
-            <InputNumber v-model="game.player1Score" input-class="w-1/2"/>
+          <FormField label="Home" class="mr-5" :error="errors.player1Score">
+            <InputNumber v-model="player1Score" input-class="w-1/2" :invalid="errors.player1Score" :min="0" :max="8"/>
           </FormField>
-          <span class="text-xl my-auto">:</span>
-          <FormField label="Away" class="ml-5">
-            <InputNumber v-model="game.player2Score" input-class="w-1/2" />
+          <span class="text-xl mt-1">:</span>
+          <FormField label="Away" class="ml-5" :error="errors.player2Score">
+            <InputNumber v-model="player2Score" input-class="w-1/2" :invalid="errors.player2Score" :min="0" :max="8"/>
           </FormField>
         </div>
       </div>
@@ -44,11 +48,14 @@
 <script setup lang="ts">
 import { createGame } from "../../api.ts";
 import FormField from "../utility/FormField.vue";
-import {computed, ref} from "vue";
+import {computed} from "vue";
 import {useUserStore} from "../../composables/store/useUserStore.ts";
 import {Game, Location, User} from "../../types/types.ts";
 import {useReferenceDataStore} from "../../composables/store/useReferenceDataStore.ts";
 import {useGameStore} from "../../composables/store/useGameStore.ts";
+import { useForm } from "vee-validate";
+import { toTypedSchema } from '@vee-validate/zod';
+import { z } from 'zod';
 import {storeToRefs} from "pinia";
 
 const { id, user, isAuthenticated } = storeToRefs(useUserStore())
@@ -56,38 +63,46 @@ const { setGames, allGames } = useGameStore()
 const { getLocations, getUsers } = useReferenceDataStore()
 
 const opponents = getUsers?.filter((opponent) => opponent.id !== id.value)
-
 const visible = defineModel<boolean>()
 
-interface GameDetails {
-  location: Location | null,
-  player1: User | string |null,
-  player2: User | string | null,
-  player1BallColour: string | null,
-  player1Score: number | null,
-  player2Score: number | null,
-}
+const validationSchema = toTypedSchema(
+    z.object({
+      player1: z.custom<User | string>(),
+      player2: z.custom<User | string>(),
+      location: z.custom<Location>(),
+      player1BallColour: z.string(),
+      player1Score: z.number({ required_error: "Required", invalid_type_error: "Required"}).lte(8, { message: "Score should be between 0-8" }).gte(0, { message: "Score should be between 0-8" }),
+      player2Score: z.number({ required_error: "Required", invalid_type_error: "Required" }).lte(8, { message: "Score should be between 0-8"}).gte(0, { message: "Score should be between 0-8" })
+    })  .required()
+        .refine((obj) => obj.player1Score + obj.player2Score < 16)
+)
 
-const game = ref<GameDetails>({
-  location: null,
-  player1: null,
-  player2: null,
-  player1BallColour: null,
-  player1Score: null,
-  player2Score: null
+const { errors, defineField, validate } = useForm({
+  validationSchema
 })
 
-const ballColours = computed<string[]>(() => game.value.location !== null ? getLocations.find((loc) => loc.id === game.value.location?.id)?.ballColours : [])
+const [player1] = defineField('player1')
+const [player2] = defineField('player2')
+const [location] = defineField('location')
+const [player1BallColour] = defineField('player1BallColour')
+const [player1Score] = defineField('player1Score')
+const [player2Score] = defineField('player2Score')
+
+const ballColours = computed<string[]>(() => location.value !== null ? getLocations.find((loc) => loc.id === location.value?.id)?.ballColours : [])
 
 const registerGame = async () => {
+  if(await validate()){
+    return false;
+  }
+  
   const gameToInsert = {
-    player1: isAuthenticated.value ? user.value : game.value.player1,
-    player2: game.value.player2,
-    player1BallColour: game.value.player1BallColour,
-    player2BallColour: game.value.location!.ballColours.find(colour => colour !== game.value.player1BallColour),
-    location: game.value.location,
-    player1Score: game.value.player1Score,
-    player2Score: game.value.player2Score
+    player1: isAuthenticated.value ? user.value : player1.value,
+    player2: player2.value,
+    player1BallColour: player1BallColour.value,
+    player2BallColour: location.value!.ballColours!.find(colour => colour !== player1BallColour.value),
+    location: location.value,
+    player1Score: player1Score.value,
+    player2Score: player2Score.value
   } as Game
 
   if(isAuthenticated.value) {
@@ -95,16 +110,7 @@ const registerGame = async () => {
   }
 
   setGames([...allGames, gameToInsert])
-
-  game.value = {
-    location: null,
-    player1: null,
-    player2: null,
-    player1BallColour: null,
-    player1Score: null,
-    player2Score: null
-  }
-
+  
   visible.value = false
 }
 </script>
--- src/components/utility/FormField.vue
+++ src/components/utility/FormField.vue
@@ -1,15 +1,20 @@
 <template>
-  <FloatLabel>
-    <slot :id="label" />
-    <label :for="label">{{ label}}</label>
-  </FloatLabel>
+  <div class="flex flex-col">
+    <FloatLabel>
+      <slot :id="label" />
+      <label :for="label">{{ label }}</label>
+    </FloatLabel>
+    <span class="text-xs mt-2 ml-3 text-red-400">{{  error }}</span>
+  </div>
+
 </template>
 
 <script setup lang="ts">
 import FloatLabel from 'primevue/floatlabel';
 
 interface FormFieldProps {
-  label: string
+  label: string,
+  error: string
 }
 
 const { label } = defineProps<FormFieldProps>()
