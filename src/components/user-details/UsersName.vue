4f33eff6b257ebfe88e3f609311ec2b6c10bc370
Jake <jacobminor@hotmail.co.uk>
2024-04-14 22:41:33 +0100

    Update user details to render avatar when url is present

--- src/components/user-details/UsersName.vue
+++ src/components/user-details/UsersName.vue
@@ -1,8 +1,8 @@
 ﻿<template>
   <div class="flex cursor-pointer" @click="dropdownOpen = !dropdownOpen">
     <Icon :icon="dropdownOpen ? 'ion:chevron-up-outline' : 'ion:chevron-down-outline'" class="my-auto mr-1" />
-    <div class="mr-5 my-auto">{{ store.name }}</div>
-    <Image :src="store.avatarUrl" width="50" image-class="rounded-full" />
+    <div class="mr-5 my-auto">{{ name }}</div>
+    <Image :src="avatarUrl" v-if="avatarUrl" width="50" image-class="rounded-full" />
   </div>
   <ProfileDropdown :dropdown-open="dropdownOpen" />
 </template>
@@ -12,8 +12,9 @@ import { Icon } from "@iconify/vue";
 import { ref } from "vue";
 import { useUserStore } from "../../composables/store/useUserStore.ts";
 import ProfileDropdown from "./ProfileDropdown.vue";
+import {storeToRefs} from "pinia";
 
 const dropdownOpen = ref(false)
-const store = useUserStore()
+const { avatarUrl, name } = storeToRefs(useUserStore())
 
 </script>
